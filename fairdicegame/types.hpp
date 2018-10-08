2b2e9efd37d1c6d2a1c0f26ceeadbfd1282a62bd
niuyg <y-g.niu@163.com>
2018-10-08 14:18:18 +0000

    go back to dappub origin code 20181008

--- fairdicegame/types.hpp
+++ fairdicegame/types.hpp
@@ -1,44 +1,45 @@
 #include "utils.hpp"
 
-#define MAIN_SYMBOL S(4, SYS)
-#define REVEALER N(niuaaaaaaaaa)
-#define LOG N(niufffffffff)
+#define EOS_SYMBOL S(4, EOS)
+#define REVEALER N(gamerevealer)
+#define LOG N(fairdicelogs)
 
 static const string PUB_KEY =
     "EOS4wpa8kRBbZof9JEpPAAgbnN65NhbBobW4x5gyxQoFfamhQCAMX";
 
 // @abi table bets i64
 struct st_bet {
-    uint64_t     id;
+    uint64_t id;
     account_name player;
     account_name referrer;
-    asset        amount;
-    uint8_t      roll_under;
-    checksum256  seed_hash;
-    checksum160  user_seed_hash;
-    uint64_t     created_at;
-    uint64_t     primary_key() const { return id; }
+    asset amount;
+    uint8_t roll_under;
+    checksum256 seed_hash;
+    checksum160 user_seed_hash;
+    uint64_t created_at;
+    uint64_t primary_key() const { return id; }
 };
 
 struct st_result {
-    uint64_t     bet_id;
+    uint64_t bet_id;
     account_name player;
     account_name referrer;
-    asset        amount;
-    uint8_t      roll_under;
-    uint8_t      random_roll;
-    checksum256  seed;
-    checksum256  seed_hash;
-    checksum160  user_seed_hash;
-    asset        payout;
+    asset amount;
+    uint8_t roll_under;
+    uint8_t random_roll;
+    checksum256 seed;
+    checksum256 seed_hash;
+    checksum160 user_seed_hash;
+    asset payout;
 };
 
 // @abi table hash i64
 struct st_hash {
     checksum256 hash;
-    uint64_t    expiration;
-    uint64_t    primary_key() const { return uint64_hash(hash); }
-    uint64_t    by_expiration() const { return expiration; }
+    uint64_t expiration;
+    uint64_t primary_key() const { return uint64_hash(hash); }
+
+    uint64_t by_expiration() const { return expiration; }
 };
 
 // @abi table fundpool i64
@@ -51,14 +52,13 @@ struct st_global {
     uint64_t current_id;
 };
 
-typedef multi_index<N(bets),   st_bet>       tb_bets;
+typedef multi_index<N(bets), st_bet>
+    tb_bets;
 typedef singleton<N(fundpool), st_fund_pool> tb_fund_pool;
-typedef singleton<N(global),   st_global>    tb_global;
+typedef singleton<N(global), st_global> tb_global;
 typedef multi_index<
     N(hash),
     st_hash,
-    indexed_by<
-        N(by_expiration),
-        const_mem_fun<st_hash, uint64_t, &st_hash::by_expiration>
-    >
-> tb_hash;
+    indexed_by<N(by_expiration),
+               const_mem_fun<st_hash, uint64_t, &st_hash::by_expiration>>>
+    tb_hash;
